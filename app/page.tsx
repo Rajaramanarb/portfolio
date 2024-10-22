4f54a5ed4702cc0dc88d4ab50720a008dbd2fcd0
Rajaramanarb <rajaramanarb@gmail.com>
2024-10-22 13:20:47 +0530

    Removed experience

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         {/* <Clients /> */}
-        <Experience />
+        {/* <Experience /> */}
         <Approach />
         <Footer />
       </div>
